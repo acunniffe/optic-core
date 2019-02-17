050e935612140bb080d86a55ccfa70ed72e00723
Dev Doshi <dev@devdoshi.com>
2019-02-17 13:56:08 -0500

    moved config from optic-cli

---
+++ src/optic-config.ts
@@ -0,0 +1,61 @@
+import * as Joi from 'joi';
+import { IApiMeta, ISessionManagerOptions } from './session-manager';
+
+const opticCoreVersion = require('../package.json').version;
+
+export const opticYamlFileName = 'optic.yml';
+
+export const strategyConfigType = Joi.alternatives(
+  Joi.object().keys({
+    type: Joi.string().valid('logging').required(),
+    commandToRun: Joi.string().required(),
+  }),
+  Joi.object().keys({
+    type: Joi.string().valid('proxy'),
+    commandToRun: Joi.string().required(),
+    targetHost: Joi.string().required(),
+    targetPort: Joi.number().required(),
+  }),
+);
+
+export const securityConfigType = Joi.array()
+  .items(
+    Joi.object().keys({
+      type: Joi.string().valid('basic', 'bearer').required(),
+    }),
+    Joi.object().keys({
+      type: Joi.string().valid('apiKey').required(),
+      in: Joi.string().valid('cookie', 'query', 'header').required(),
+      name: Joi.string().required().min(1),
+    }),
+  );
+
+export const opticConfigType = Joi.object()
+  .keys({
+    strategy: strategyConfigType,
+    api: Joi.object().keys({
+      id: Joi.string().required(),
+      security: securityConfigType,
+      paths: Joi.array().items(Joi.string()).required(),
+    }).required(),
+    optic: Joi.object().keys({
+      version: Joi.string().default(opticCoreVersion).required(),
+      apiBaseUrl: Joi.string().default('https://api.useoptic.com').required(),
+      baseUrl: Joi.string().default('https://useoptic.com').required(),
+    }),
+  });
+
+export function validate(config: object) {
+  return Joi.validate(config, opticConfigType)
+}
+
+export interface IOpticYamlConfig extends ISessionManagerOptions {
+  api: {
+    id: string
+  } & IApiMeta
+  optic: {
+    version: string
+    apiBaseUrl: string
+    baseUrl: string
+  }
+}
